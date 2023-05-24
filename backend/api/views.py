c90de50e91939fb4630db7112845b7e01ad26b4d
evi1ghost <evil_ghost@bk.ru>
2023-05-24 12:16:28 +0300

    fix tasks removal

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Task
 from .serializers import TaskSerializer
@@ -7,3 +9,8 @@ from .serializers import TaskSerializer
 class TaskView(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+
+    def destroy(self, *args, **kwargs):
+        serializer = self.get_serializer(self.get_object())
+        super().destroy(*args, **kwargs)
+        return Response(serializer.data, status=status.HTTP_200_OK)
