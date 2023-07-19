bc7653d82621232c377680d1047aecff62dfc13b
Иван Сапожников <119330332+8312-ru@users.noreply.github.com>
2023-07-19 03:00:52 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.71.254', '127.0.0.1', 'localhost', 'taski52.sytes.net']
 
 
 # Application definition
