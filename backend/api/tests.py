e9ffbb271369d69868cfd6bde9c18477269255e2
Far-001 <Korchagin.86@mail.ru>
2023-07-30 17:27:34 +0700

    Add tests 01

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,9 +1,8 @@
 # backend/api/tests.py
 from http import HTTPStatus
 
-from django.test import Client, TestCase
-
 from api import models
+from django.test import Client, TestCase
 
 
 class TaskiAPITestCase(TestCase):
