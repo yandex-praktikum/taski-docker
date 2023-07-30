dc64a1910f7d530a213bfb8a13abbeed7d5cf268
Far-001 <Korchagin.86@mail.ru>
2023-07-30 17:23:56 +0700

    Add tests

---
+++ backend/api/tests.py
@@ -0,0 +1,23 @@
+# backend/api/tests.py
+from http import HTTPStatus
+
+from django.test import Client, TestCase
+
+from api import models
+
+
+class TaskiAPITestCase(TestCase):
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_list_exists(self):
+        """Проверка доступности списка задач."""
+        response = self.guest_client.get('/api/tasks/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_task_creation(self):
+        """Проверка создания задачи."""
+        data = {'title': 'Test', 'description': 'Test'}
+        response = self.guest_client.post('/api/tasks/', data=data)
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
+        self.assertTrue(models.Task.objects.filter(title='Test').exists())
