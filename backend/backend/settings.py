c9b22610bda99e4709bbda671ab8c6eeabce559a
Pako1979vl <pako1979@yandex.ru>
2023-08-10 14:00:38 +0300

    Внесены изм. Собрана статика бека - работает верно

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,6 +111,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / 'collected_static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
