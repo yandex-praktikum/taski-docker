310de60abc8586b29c0790ca5839c6bf70bf35f2
Дарья Линецкая <Dasha-V-L@yandex.ru>
2023-11-17 10:15:47 +0300

    add setup.cfg

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -62,12 +62,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
+	'ENGINE': 'django.db.backends.postgresql',
+	'NAME': os.getenv('POSTGRES_DB', 'django'),
 	'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432)
+	'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+	'HOST': os.getenv('DB_HOST', ''),
+	'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
